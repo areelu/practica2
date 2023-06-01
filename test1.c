1ae8b13f01f35eaf3f04356c4a108561ed539a0b
Arely Luis <100141487+areelu@users.noreply.github.com>
2023-06-01 02:17:20 -0600

    Update test1.c

--- test1.c
+++ test1.c
@@ -15,8 +15,8 @@ int main(int argc, char **argv) {
     int width, height, channels;
     const char *name;
     size_t img_size;
-    int green_channels;
-    unsigned char *green_img = NULL;
+    int gray_channels;
+    unsigned char *gray_img = NULL;
 
     MPI_Init(&argc, &argv); /* Inicializar MPI */
     MPI_Comm_rank(MPI_COMM_WORLD, &miproc); /* Determinar el rango del proceso invocado*/
@@ -41,46 +41,44 @@ int main(int argc, char **argv) {
         printf("Imagen cargada con un ancho de %dpx, un alto de %dpx y %d canales\n", width, height, channels);
 
         img_size = width * height * channels;
-        green_channels = channels;
-        size_t green_img_size = width * height * green_channels;
+        gray_channels = channels == 4 ? 2 : 1;
+        size_t gray_img_size = width * height * gray_channels;
 
-        green_img = (unsigned char*)malloc(green_img_size);
-        if (green_img == NULL) {
-            printf("No se pudo asignar memoria para la imagen en escala de verdes.\n");
+        gray_img = (unsigned char*)malloc(gray_img_size);
+        if (gray_img == NULL) {
+            printf("No se pudo asignar memoria para la imagen en escala de grises.\n");
             MPI_Finalize();
             return 1;
         }
 
-        for (unsigned char *p = img, *pg = green_img; p != img + img_size; p += channels, pg += green_channels) {
-            *pg = 0;  // Valor de rojo
-            *(pg + 1) = *p;  // Valor de verde
-            *(pg + 2) = 0;  // Valor de azul
+        for (unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) {
+            *pg = (uint8_t)((*p + *(p + 1) + *(p + 2)) / 3.0);
             if (channels == 4) {
-                *(pg + 3) = *(p + 3);  // Valor de alfa
+                *(pg + 1) = *(p + 3);
             }
         }
     }
 
     MPI_Bcast(&img_size, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&green_channels, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&gray_channels, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (miproc != 0) {
-        green_img = (unsigned char*)malloc(img_size);
-        if (green_img == NULL) {
-            printf("No se pudo asignar memoria para la imagen en escala de verdes.\n");
+        gray_img = (unsigned char*)malloc(img_size);
+        if (gray_img == NULL) {
+            printf("No se pudo asignar memoria para la imagen en escala de grises.\n");
             MPI_Finalize();
             return 1;
         }
     }
 
-    MPI_Bcast(green_img, img_size, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Bcast(gray_img, img_size, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
     if (miproc == 0) { // Proceso maestro
-        stbi_write_png("Shapes_green.png", width, height, green_channels, green_img, width * green_channels);
+        stbi_write_png("flor.png", width, height, gray_channels, gray_img, width * gray_channels);
         stbi_image_free(img);
-        free(green_img);
+        free(gray_img);
     } else {
-        free(green_img);
+        free(gray_img);
     }
 
     MPI_Finalize();
