b199ab3cd2ee56f5c09b2d440399875b357d9ea1
Arely Luis <100141487+areelu@users.noreply.github.com>
2023-06-01 01:18:36 -0600

    Create myvar.h

---
+++ myvar.h
@@ -0,0 +1,21 @@
+#ifndef MYVAR
+#define MYVAR
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+typedef struct{
+  double a;
+  double b;
+  double dt;
+}MPI_myvar;
+  
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* myvar */
+
